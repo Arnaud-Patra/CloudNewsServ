abff10ac3fd0a0b43cfbff5faca67fdaeb2e99f4
Arnaud-Patra <tnguyen@keluro.com>
2019-12-29 16:18:55 +0100

    allow cross origins

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 from fetcher import main
 
@@ -9,25 +10,33 @@ from fetcher import main
 
 # flask used as a API
 app = Flask(__name__)
+# cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/get_france')
+@cross_origin()
 def get_france():
     articles = main(['FRANCE'])
     return articles
 
 
 @app.route('/get_tech')
+@cross_origin()
 def get_tech():
     articles = main(['TECH'])
     return articles
 
 
 @app.route('/get_reddit_news')
+@cross_origin()
 def get_reddit_news():
     articles = main(['REDDIT_NEWS'])
     return articles
 
+
 @app.route('/helloworld')
+@cross_origin()
 def helloworld():
     return "hello"
 
